4cad96dc86be42c1803b63331b91e450ceff44dd
Sandeep Tc <sandeeptc@Sandeeps-MacBook-Pro.local>
2021-05-30 19:21:47 +0530

    refactor: adding some more changes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -49,3 +49,15 @@ export const deletePost = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 }
+
+export const likePost = async (req, res) => {
+    const { id: _id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post with that Id')
+    try {
+        const post = await PostMessage.findById(_id)
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, { likeCount: post.likeCount + 1 }, { new: true })
+        res.json(updatedPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express()
-
+dotenv.config()
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -15,10 +15,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://sandeep:Radial10@cluster0.h4lci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://sandeep:Radial10@cluster0.h4lci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server running on ${PORT}`)))
     .catch((error) => console.log(error.message))
 
